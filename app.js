33add183f7bf0730c9935ac842ca28f3b80b7cc2
Liam DeWitt <mugzy550@gmail.com>
2023-03-21 16:43:49 -0400

    add new variable for gameover status and change logic for when player reaches score to win

--- app.js
+++ app.js
@@ -6,23 +6,34 @@ const p2Display = document.querySelector('#p2Display');
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click',()=>{
     console.log('clicked p1b')
-    if (p1Score !== winningScore){
+    if(!isGameOver){
         p1Score += 1
         //same thing as ++
-        p1Display.textContent.Content = p1Score
+        if(p1Score === winningScore){
+            isGameOver = true;
+        }
+        p1Display.textContent = p1Score
         //innerText and textContent or similar but remember, innerText only displays
         //the visible text to the viewer, nothing hidden.
-    };
+    }
     
 })
 p2Button.addEventListener('click',()=>{
     console.log('clicked p2b')
-    if (p1Score !== winningScore){
-        p2Score ++
+    if(!isGameOver){
+        p2Score += 1
         //same thing as ++
-        p2Display.textContent.Content = p2Score
-    };
+        if(p2Score === winningScore){
+            isGameOver = true;
+        }
+        p2Display.textContent = p2Score
+        //innerText and textContent or similar but remember, innerText only displays
+        //the visible text to the viewer, nothing hidden.
+    }
+    
 })
+
